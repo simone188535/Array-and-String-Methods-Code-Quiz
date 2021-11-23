5a2fa6e55a2709ef3f744975fb3b7c7334a52fd2
Simone Anthony <simone.anthony1@yahoo.com>
2021-11-23 18:25:55 -0500

    timer corrected

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,16 +1,17 @@
 const timeRemainingEl = document.querySelector('#time-remaining');
 const startButtonEl = document.querySelector('.start-button');
-let remainingTime = 100;
+let remainingTime = 10;
 
 // could use ternary here if startButtonEl is presetn in page
+timeRemainingEl.textContent = remainingTime;
 let timer = setInterval(countdown, 1000);
 
 function countdown() {
-    timeRemainingEl.textContent = remainingTime;
     remainingTime--;
 
     if (remainingTime === 0) {
        //  exit quiz 
-       clearTimeout(timer);
+       clearTimeout(timer); 
     }
+    timeRemainingEl.textContent = remainingTime;
 }
